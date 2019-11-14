27168649b48991e81807904ecc4d5fae6dd9e287
pbeicn <pbeicn@qq.com>
2019-11-14 13:33:18 +0800

    6666

--- src/views/saoma.vue
+++ src/views/saoma.vue
@@ -12,44 +12,167 @@
           <a-input class="ainput" placeholder="请先扫获取患者授权，查询患者最近三次历史就诊记录" />
         </div>
         <div class="pname">
-          <a-button type="primary">扫码</a-button>
+          <a-button type="primary" @click="getRecentDiagnose">扫码</a-button>
         </div>
         <div class="pname">
           <a-button type="primary">录入诊断</a-button>
         </div>
       </div>
-      <div class="recent bg">
-        <div class="incontent bg2">
+      <div class="recent">
+        <div class="incontent">
           <a-card
             title
             :bordered="true"
-            class="css1"
-            v-for="(checks,index) in tableData"
+            v-for="(recent,index) in recents"
             :key="index"
+            :class="index===0?'css1':'css2'"
           >
             <div>
-              <div class="text">项目名称：</div>
+              <div>
+                {{ recent.docname }}
+                <span style="width:20px"></span>
+                {{ recent.docdept }}
+              </div>
             </div>
             <div>
-              <div class="text">检查科室：/div>
+              <div>{{ recent.starttime }}</div>
             </div>
             <div>
-              <div class="text">检查时间：</div>
+              <div class="text">
+                {{ recent.result }}
+                <span style="width:20px"></span>
+                {{ recent.totalmoney }}
+              </div>
             </div>
           </a-card>
+        </div>
+      </div>
+      <div class="maininfo">
+        <div class="recipe">
+          <div style="text-align:left">
+            <a-tabs defaultActiveKey="1">
+              <a-tab-pane tab="处方" key="1">
+                <a-table
+                  :columns="columns"
+                  :dataSource="recipedata"
+                  :rowKey="recipedata.key"
+                  :pagination="pagination"
+                  :loading="loading"
+                >
+                  <template slot="money" slot-scope="text">￥{{ text }}</template>
+                  <template slot="allmoney" slot-scope="text">￥{{ text }}</template>
+                </a-table>
+                <div class="recipeamount">
+                  合计：
+                  <span style="color: red;">￥{{ recipeamount }}</span>
+                </div>
+              </a-tab-pane>
+            </a-tabs>
+          </div>
+        </div>
+        <div style="height:30px"></div>
+        <div class="recipe">
+          <div style="text-align:left">
+            <a-tabs defaultActiveKey="1">
+              <a-tab-pane tab="检查" key="1">
+                <div class="checkcss">
+                  <a-card
+                    title
+                    :bordered="true"
+                    class="css1"
+                    v-for="(checks,index) in checkdtos"
+                    :key="index"
+                  >
+                    <div>
+                      <img src="@/assets/c1.png" />
+                      <div class="text">项目名称：{{ checks.description }}(费用:￥{{ checks.money }})</div>
+                    </div>
+                    <div>
+                      <img src="@/assets/c2.png" />
+                      <div class="text">检查科室：{{ checks.department }}</div>
+                    </div>
+                    <div>
+                      <img src="@/assets/c3.png" />
+                      <div class="text">检查时间：{{checks.patientage}}</div>
+                    </div>
+                  </a-card>
+                </div>
+                <div class="recipeamount">
+                  合计：
+                  <span style="color: red;">￥{{ checkamount }}</span>
+                </div>
+              </a-tab-pane>
+            </a-tabs>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+const columns = [
+  {
+    title: "药品名称",
+    dataIndex: "medicinename",
+    // width: "20%",
+    scopedSlots: { customRender: "medicinename" }
+  },
+  {
+    title: "品牌",
+    dataIndex: "brandname",
+    scopedSlots: { customRender: "brandname" }
+  },
+  {
+    title: "出库包装",
+    dataIndex: "sellpackage",
+    scopedSlots: { customRender: "sellpackage" }
+  },
+  {
+    title: "药品剂型",
+    dataIndex: "dosageform",
+    scopedSlots: { customRender: "dosageform" }
+  },
+  {
+    title: "剂量/次",
+    dataIndex: "dosesiz",
+    scopedSlots: { customRender: "dosesiz" }
+  },
+  {
+    title: "使用频次",
+    dataIndex: "frequency",
+    scopedSlots: { customRender: "frequency" }
+  },
+  {
+    title: "药品数量",
+    dataIndex: "medicinecnt",
+    scopedSlots: { customRender: "medicinecnt" }
+  },
+  {
+    title: "药品价格",
+    dataIndex: "money",
+    scopedSlots: { customRender: "money" }
+  },
+  {
+    title: "费用小计",
+    dataIndex: "allmoney",
+    scopedSlots: { customRender: "allmoney" }
+  }
+];
 import DoctorHeader from "@/components/header/doc-header";
 export default {
   components: { DoctorHeader },
   data() {
     return {
       name: "",
-      department: ""
+      department: "",
+      recents: [],
+      recipedata: [],
+      checkdtos: [],
+      pagination: {},
+      loading: false,
+      columns,
+      recipeamount: 0,
+      checkamount: 0
     };
   },
   created() {
@@ -65,6 +188,20 @@ export default {
     // }
   },
   watch: {
+    recipedata(newValue) {
+      let amount = 0;
+      newValue.map(item => {
+        amount = amount + parseFloat(item.allmoney);
+      });
+      this.recipeamount = amount.toFixed(2);
+    },
+    checkdtos(newValue) {
+      let amount = 0;
+      newValue.map(item => {
+        amount = amount + parseFloat(item.money);
+      });
+      this.checkamount = amount.toFixed(2);
+    }
     // patients: {
     //   handler(newValue, oldValue) {
     //     for (let i = 0; i < newValue.length; i++) {
@@ -78,6 +215,7 @@ export default {
   },
   methods: {
     getDoctorAndPatients() {
+      //查询医生信息
       this.$ajax
         .get("api/v1/doctor1")
         .then(res => {
@@ -88,6 +226,29 @@ export default {
         .catch(res => {
           window.console.log(res);
         });
+    },
+    getRecentDiagnose() {
+      this.recents = [];
+      //查询医生信息
+      this.$ajax
+        .get("/api/v1/diagnose/recent")
+        .then(res => {
+          window.console.log(res);
+          this.recents = res.data.list;
+          this.checkdtos = res.data.list[0].checkDTOS;
+          this.getSingleMoney(res.data.list[0].recipe.medicine);
+          //   this.recipedata = res.data.list[0].recipe.medicine;
+        })
+        .catch(res => {
+          window.console.log(res);
+        });
+    },
+    getSingleMoney(colsss) {
+      for (let i = 0; i < colsss.length; i++) {
+        const element = colsss[i];
+        element.allmoney = (element.medicinecnt * element.money).toFixed(2);
+      }
+      this.recipedata = colsss;
     }
   }
 };
@@ -139,12 +300,70 @@ export default {
     }
     .recent {
       width: 100%;
-      height: 60px;
+      height: 180px;
       display: flex;
       justify-content: center;
       align-items: center;
       .incontent {
         width: 1000px;
+        display: flex;
+        // justify-content: space-between;
+        .css1 {
+          width: 300px;
+          height: 145px;
+          background: rgba(135, 182, 242, 1);
+          box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.2);
+          border-radius: 2px;
+          color: white;
+          margin: 20px;
+          div {
+            padding-bottom: 10px;
+            height: 35px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            .text {
+              font-size: 16px;
+              font-family: PingFangSC-Regular, PingFang SC;
+              font-weight: 400;
+              color: rgba(255, 255, 255, 1);
+              line-height: 28px;
+            }
+          }
+        }
+        .css2 {
+          width: 300px;
+          height: 145px;
+          background: rgba(255, 255, 255, 1);
+          box-shadow: 0px 2px 8px 1px rgba(0, 0, 0, 0.09);
+          border-radius: 2px;
+          margin: 20px;
+          div {
+            padding-bottom: 10px;
+            height: 35px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            .text {
+              font-size: 16px;
+              font-family: PingFangSC-Regular, PingFang SC;
+              font-weight: 400;
+              line-height: 28px;
+            }
+          }
+        }
+      }
+    }
+    .maininfo {
+      width: 100%;
+      min-height: 1200px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      .recipe {
+        width: 1000px;
+        min-height: 547px;
+        background: rgba(255, 255, 255, 1);
       }
     }
   }
@@ -159,5 +378,50 @@ export default {
   margin: 18px;
   display: flex;
   flex-direction: row;
+}
+.recipeamount {
+  text-align: right;
+  margin: 15px;
+  font-size: 16px;
+  font-weight: 400;
+  /*color: rgba(255, 91, 91, 1);*/
+  color: rgba(0, 0, 0, 0.65);
+  line-height: 21px;
+}
+.checkcss {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  .css1 {
+    width: 299px;
+    height: 142px;
+    margin-left: 30px;
+    margin-top: 20px;
+    box-shadow: 0px 2px 8px 1px rgba(0, 0, 0, 0.09);
+    position: relative;
+    div {
+      padding-bottom: 10px;
+      height: 35px;
+      display: flex;
+      flex-direction: row;
+      align-items: center; /*垂直居中*/
+      // justify-content: center; /*水平居中*/
+      img {
+        vertical-align: middle;
+        align-items: center;
+      }
+      .text {
+        flex: 1;
+        margin-top: 10px;
+        margin-left: 5px;
+      }
+    }
+    .inc1 {
+      position: absolute;
+      right: 10px;
+      bottom: 10px;
+    }
+  }
 }
 </style>
